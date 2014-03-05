9c7789f8da612dd952f8673a7405a6adc9f657bd
Zhiwei YAN <jerod.yan@gmail.com>
2014-03-05 18:05:00 +0800

    init

---
+++ latex_temp_file_clean.bat
@@ -0,0 +1,6 @@
+REM ÇåÀíÄ¿Â¼
+
+del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
+
+del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi .DS_Store
+del /S /F *.brf *.nls *.nlo *.synctex.gz *.synctex.gz(busy) *.synctex
