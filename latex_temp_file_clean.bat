8fbc99254b4f2a978bf30a1a6b7ef145a68b4791
Zhiwei YAN <jerod.yan@gmail.com>
2014-04-02 13:49:29 +0800

    push back

--- latex_temp_file_clean.bat
+++ latex_temp_file_clean.bat
@@ -3,4 +3,4 @@ REM ÇåÀíÄ¿Â¼
 del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
 
 del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi .DS_Store
-del /S /F *.brf *.nls *.nlo *.synctex.gz *.synctex.gz(busy) *.synctex
+del /S /F *.brf *.nls *.nlo *.synctex.gz *.synctex.gz(busy) *.synctex *.log
